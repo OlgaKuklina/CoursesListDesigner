91dec3b13d999810ded1db9a4e3d357c115e47f2
OlgaKuklina <okuklina@hotmail.com>
2016-06-11 20:32:02 -0700

    - first commit

---
+++ src/CourseListGenerator.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CourseListGenerator {
+
+    public static void main(String[] args) throws IOException {
+        String path = System.getProperty("user.dir") + "/resourses/courses.txt";
+        HashMap<String, String> courseMap = new HashMap<>();
+        HashMap<String, List<String>> dependencyMap = new HashMap<>();
+        BufferedReader fileReader = new BufferedReader(new FileReader(path));
+        try {
+            String readLine;
+            String currentCourse = null;
+            while ((readLine = fileReader.readLine()) != null) {
+                if (readLine.equals("")) {
+                    continue;
+                }
+                if (readLine.startsWith("HCDE")) {
+                    int index = readLine.indexOf(" ", 5);
+                    currentCourse = readLine.substring(5, index);
+                    courseMap.put(currentCourse, readLine.substring(index + 1));
+                }
+                if (readLine.startsWith("Course")) {
+                    int index = readLine.indexOf(":") + 1;
+                    if (readLine.substring(index).isEmpty()) {
+                        dependencyMap.put(currentCourse, Collections.emptyList());
+                        continue;
+                    }
+                    LinkedList list = new LinkedList();
+                    int index2 = readLine.indexOf(",", index);
+                    if (index2 == -1) {
+                        if (readLine.substring(index).isEmpty()) {
+                            continue;
+                        } else {
+                            list.add(readLine.substring(index));
+                        }
+                    } else {
+                        while (index < readLine.length() && index2 < readLine.length()) {
+                            list.add(readLine.substring(index, index2));
+                            index = index2 + 1;
+                            index2 = readLine.indexOf(",", index);
+                            if (index2 == -1) {
+                                if (readLine.substring(index).isEmpty()) {
+                                    break;
+                                } else {
+                                    list.add(readLine.substring(index));
+                                    break;
+                                }
+                            }
+                        }
+
+                    }
+                    dependencyMap.put(currentCourse, list);
+                }
+            }
+        } finally {
+            fileReader.close();
+        }
+        List<String> courseSequence = courseSequenceRecommendation(dependencyMap);
+        returnRecommendedList(courseMap, courseSequence);
+    }
+
+
+    public static List<String> courseSequenceRecommendation(HashMap<String, List<String>> courseDepMap) {
+        Collection<String> courseSequence = new LinkedHashSet<>();
+
+        for (String courseId : courseDepMap.keySet()) {
+            getAllSequence(courseId, courseDepMap, courseSequence);
+        }
+        return courseSequence.stream().collect(Collectors.toList());
+    }
+
+    public static void getAllSequence(String courseId, HashMap<String, List<String>> courseDepMap, Collection<String> courseSequence) {
+        if (courseDepMap.get(courseId) == null) {
+            if (!courseSequence.contains(courseId)) {
+                courseSequence.add(courseId);
+            }
+            return;
+        }
+        Collection<String> depList = courseDepMap.get(courseId);
+        for (String depId : depList) {
+            getAllSequence(depId, courseDepMap, courseSequence);
+        }
+        if (!courseSequence.contains(courseId)) {
+            courseSequence.add(courseId);
+        }
+    }
+
+    public static void returnRecommendedList(HashMap<String, String> courseMap, Collection<String> courseSequence) {
+        for (String courseId : courseSequence) {
+            if (courseMap.containsKey(courseId)) {
+                System.out.println(courseId + " " + courseMap.get(courseId));
+            }
+        }
+
+    }
+}
